6c22949d2804ac5e0c3221ec09110d98445f632b
Nascccom <ostrov_o@bk.ru>
2024-02-18 16:53:24 +0300

    Rewritten to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,49 @@
 import React from 'react';
-import styles from "./components/Site.module.css";
-import {Navigate, NavLink, Route, Routes, useNavigate} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
 import {Error404} from "./components/pages/Error404";
+import {S} from "./components/pages/_styles";
 
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR404: '/error404'
+} as const
 
 function App() {
-    const navigate = useNavigate()
 
     return (
       <div>
-          <div className={styles.header}><h1>HEADER</h1></div>
-          <div className={styles.body}>
-              <div className={styles.nav}>
-                  <div>
-                      <NavLink to='/page1'
-                               className={({isActive}) => isActive ? styles.active : styles.navLink}>
-                          Page 1
-                      </NavLink>
-                  </div>
-                  <div>
-                      <NavLink to='/page2' className={({isActive}) => isActive ? styles.active : styles.navLink}>
-                          Page 2
-                      </NavLink>
-                  </div>
-                  <div>
-                      <NavLink to='/page3' className={({isActive}) => isActive ? styles.active : styles.navLink}>
-                          Page 3
-                      </NavLink>
-                  </div>
-              </div>
-              <div className={styles.content}>
+          <S.Header><h1>HEADER</h1></S.Header>
+          <S.Body>
+              <S.Nav>
+                  <S.NavWrapper>
+                      <NavLink to={PATH.PAGE1}>Page 1</NavLink>
+                  </S.NavWrapper>
+                  <S.NavWrapper>
+                      <NavLink to={PATH.PAGE2}>Page 2</NavLink>
+                  </S.NavWrapper>
+                  <S.NavWrapper>
+                      <NavLink to={PATH.PAGE3}>Page 3</NavLink>
+                  </S.NavWrapper>
+              </S.Nav>
+              <S.Content>
                   <Routes>
-                      <Route path='/' element={<Navigate to="/page1"/>}/>
+                      <Route path='/' element={<Navigate to={PATH.PAGE1}/>}/>
 
-                      <Route path='/page1' element={<PageOne/>}/>
-                      <Route path='/page2' element={<PageTwo/>}/>
-                      <Route path='/page3' element={<PageThree/>}/>
+                      <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                      <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                      <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
-                      <Route path='/error404' element={<Error404/>}/>
-                      <Route path='/*' element={<Navigate to="/error404"/>}/>
+                      <Route path={PATH.ERROR404} element={<Error404/>}/>
+                      <Route path='/*' element={<Navigate to={PATH.ERROR404}/>}/>
                   </Routes>
-              </div>
-          </div>
-          <div className={styles.footer}>abibas 2023</div>
+              </S.Content>
+          </S.Body>
+          <S.Footer>abibas 2023</S.Footer>
       </div>
     );
 }
